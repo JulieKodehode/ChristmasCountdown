a06d028b95a460306e110e6357a1496f4a316fa9
JulieKodehode <julie.kodehode@gmail.com>
2023-01-05 12:57:11 +0100

    *Fix: comment out console.logs

--- src/App.js
+++ src/App.js
@@ -14,15 +14,15 @@ import CountdownTimer from "./components/Countdown/CountdownTimer";
 const App = () => {
 	// Somehow this is NaN???
 	const timeToChristmas = new Date("12/24/2023").getTime();
-	console.log(timeToChristmas); // Sat Dec 23 2023 00:00:00 GMT+0100 (Central European Standard Time) // 1703372400000
+	// console.log(timeToChristmas); // Sat Dec 23 2023 00:00:00 GMT+0100 (Central European Standard Time) // 1703372400000
 
 	// getTime() Gets the current time in milliseconds.
 	const timeNow = new Date().getDate();
-	console.log(timeNow); // 1672746372692 // 5
+	// console.log(timeNow); // 1672746372692 // 5
 
 	// timeNow + timeToChristmas = christmasTime aka SANTA IS CUMMING
 	const christmasTime = timeNow + timeToChristmas;
-	console.log(christmasTime); // 1703372400005
+	// console.log(christmasTime); // 1703372400005
 
 	return (
 		<main className="mainPage">
