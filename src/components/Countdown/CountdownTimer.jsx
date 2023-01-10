32eabf9101c41c213e4a56ad558c81b89d889e1e
JulieKodehode <julie.kodehode@gmail.com>
2023-01-10 09:04:22 +0100

    Cleaning up the countdown timer

--- src/components/Countdown/CountdownTimer.jsx
+++ src/components/Countdown/CountdownTimer.jsx
@@ -6,8 +6,21 @@ import "./styling.css";
 
 // Components
 import { useCountdown } from "../../hooks/useCountdown";
-import DateTimeDisplay from "./DateTimeDisplay";
 
+// Step 1
+/**
+ * How the individual value and type is counted down.
+ */
+const DateTimeDisplay = ({ value, type }) => {
+	return (
+		<div className={"countdown"}>
+			<p>{value}</p>
+			<span>{type}</span>
+		</div>
+	);
+};
+
+// Step 2
 /**
  * This is how the value and type is displayed with individual value and type.
  */
@@ -20,7 +33,7 @@ const ShowCounter = ({ days, hours, minutes, seconds }) => {
 
 				<DateTimeDisplay value={hours} type={"Hours"} />
 
-				{/* !!For some reason minutes not part of task??? BUT INCLUDE FOR PEACE OF MIND!!! */}
+				{/* For some reason minutes not part of task??? BUT INCLUDE FOR PEACE OF MIND!!! */}
 				<DateTimeDisplay value={minutes} type={"Mins"} />
 
 				<DateTimeDisplay value={seconds} type={"Seconds"} />
@@ -29,6 +42,7 @@ const ShowCounter = ({ days, hours, minutes, seconds }) => {
 	);
 };
 
+// Step 3
 /**
  * This is the using the data from useCountdown to display the time.
  */
